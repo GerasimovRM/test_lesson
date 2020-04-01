265876c2ac885fa3cbfa9dca381e67af1a2fc669
Roman M. Gerasimov <romagrizly@gmail.com>
2020-04-01 17:01:25 +0500

    up

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def index():
 
 
 if __name__ == '__main__':
-    #port = int(os.environ.get("PORT", 5000))
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='localhost', port=port)
